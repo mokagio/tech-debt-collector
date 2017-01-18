d10aedfc8a15ae2c05be829e8e20dde1d259b68e
Giovanni Lodi <giovanni.lodi42@gmail.com>
2017-01-18 18:06:42 +1100

    Define func to get files using arr of paths regex
    We will use this to collect all the files to inspect for tech debt
    annotations.

--- lib/tech_debt_collector.rb
+++ lib/tech_debt_collector.rb
@@ -1,5 +1,9 @@
 require "tech_debt_collector/version"
 
 module TechDebtCollector
-  # Your code goes here...
+  def self.collect_file_paths(patterns)
+    patterns = [patterns] if patterns.is_a?(String)
+    return [] unless patterns.is_a?(Array)
+    Dir.glob(patterns.map { |p| File.expand_path(p, Dir.pwd) })
+  end
 end
--- spec/tech_debt_collector_spec.rb
+++ spec/tech_debt_collector_spec.rb
@@ -5,7 +5,13 @@ describe TechDebtCollector do
     expect(TechDebtCollector::VERSION).not_to be nil
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it 'returns all the files matching the given path' do
+    files = TechDebtCollector::collect_file_paths('./spec/fixtures/*.rb')
+    expect(files).to eq(["#{Dir.pwd}/spec/fixtures/a-ruby-file.rb", "#{Dir.pwd}/spec/fixtures/another-ruby-file.rb"])
+  end
+
+  it 'returns all the files matching the given paths' do
+    files = TechDebtCollector::collect_file_paths(['./spec/fixtures/*.rb', './spec/fixtures/**/*.swift'])
+    expect(files).to eq(["#{Dir.pwd}/spec/fixtures/a-ruby-file.rb", "#{Dir.pwd}/spec/fixtures/another-ruby-file.rb", "#{Dir.pwd}/spec/fixtures/a-folder/a-swift-file.swift"])
   end
 end
